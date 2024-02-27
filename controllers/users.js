abe7dc4d2b375a6133cb837ac912b446399ac63b
Oskari Pyykkönen <oskari.pyykkonen@gmail.com>
2024-02-27 12:08:11 +0200

    fix errors from model solution

--- controllers/users.js
+++ controllers/users.js
@@ -5,10 +5,16 @@ const User = require('../models/user')
 router.post('/', async (request, response) => {
   const { username, name, password } = request.body
 
-  if (!password || password.length < 3) {
+  if (!username || !password) {
     return response.status(400).json({ error: 'password missing or too short' })
   }
-
+  if (username.length < 3 || password.length < 3) {
+    return response.status(400).json({ error: 'password missing or too short' })
+  }
+  const existingUser = await User.findOne({ username })
+  if (existingUser) {
+    return response.status(400).json({ error: 'username must be unique.' })
+  }
 
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(password, saltRounds)
@@ -25,7 +31,7 @@ router.post('/', async (request, response) => {
 })
 
 router.get('/', async (request, response) => {
-  const users = await User.find({}).populate('blogs', { url: 1, title: 1, author: 1 })
+  const users = await User.find({}).populate('blogs', { url: 1, title: 1, author: 1, id: 1 })
   response.json(users)
 })
 
